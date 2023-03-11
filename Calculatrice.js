1f79fe5a2a39030ab14ba9d8d928f0f7f112deb7
Romain <romain.pietri@student.junia.com>
2023-03-11 20:10:47 +0100

    first commit

---
+++ Calculatrice.js
@@ -0,0 +1,37 @@
+function ecouteBoutons() {
+    var boutons = document.getElementsByTagName("button");
+    for (var i = 0; i < boutons.length; i++) {
+        boutons[i].onclick = function() {
+            if (this.innerHTML == "C") {
+                efface();
+            } else if (this.innerHTML == "=" ) {
+                calcul();
+            } else {
+                affiche(this.innerHTML);
+            }
+        }
+    }
+}
+function affiche(valeur) {
+    if(document.getElementById("resultat").innerHTML != ""){ efface(); }
+    var affichage = document.getElementById("calcul").innerHTML += valeur;
+    console.log(affichage);
+}
+
+function efface() {
+    document.getElementById("calcul").innerHTML="";
+    document.getElementById("resultat").innerHTML="";
+}
+
+function calcul() {
+    var affichage = document.getElementById("calcul").innerHTML;
+
+    var result =eval( affichage);
+    document.getElementById("resultat").innerHTML = result;
+    }
+
+function init() {
+    ecouteBoutons();
+    }
+
+window.onload = init;
